2435f5e24ec3390a432266cd76b096d30739aeeb
Joao Henrique Machado Silva <joaoh82@gmail.com>
2017-02-07 19:45:19 -0200

    Google Fake Search example of concurrency and channels from the Go Concurrency Pattenrs talks from Rob Pyke at Google I/O

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
+}
---
+++ search.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+type Result interface{}
+
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+func First(query string, replicas ...Search) Result {
+	c := make(chan Result)
+	searchReplica := func(i int) { c <- replicas[i](query) }
+	for i := range replicas {
+		go searchReplica(i)
+	}
+	return <-c
+}
+
+func Google(query string) (results []Result) {
+	c := make(chan Result)
+	go func() { c <- First(query, fakeSearch("web1"), fakeSearch("web2")) }()
+	go func() { c <- First(query, fakeSearch("image1"), fakeSearch("image2")) }()
+	go func() { c <- First(query, fakeSearch("video1"), fakeSearch("video2")) }()
+
+	timeout := time.After(80 * time.Millisecond)
+	for i := 0; i < 3; i++ {
+		select {
+		case result := <-c:
+			results = append(results, result)
+		case <-timeout:
+			fmt.Println("timed out")
+			return
+		}
+	}
+	return
+}
